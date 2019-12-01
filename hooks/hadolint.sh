ba261ad3d96975baf65de75d96e1e1cb3b67603f
Flaudísio Tolentino <flaudisio@gmail.com>
2019-11-30 23:37:59 -0300

    feat(hadolint): add support for --ignore
    Example usage:
    
        - repo: ...
          hooks:
            - id: hadolint
              args: [--ignore=DL3018, --ignore DL3013]
    
    which is the same of:
    
        - repo: ...
          hooks:
            - id: hadolint
              args:
                - --ignore=DL3018
                - --ignore DL3013

--- hooks/hadolint.sh
+++ hooks/hadolint.sh
@@ -4,10 +4,31 @@ set -e -u -o pipefail
 
 main()
 {
+    local hadolintOpts=""
     local error=0
 
+    while true ; do
+        case $1 in
+            --ignore*)
+                if [[ "$1" == *=* ]] ; then
+                    # Example $1: --ignore=DL3018
+                    hadolintOpts="$hadolintOpts $1"
+                else
+                    # Example $1: --ignore DL3018
+                    hadolintOpts="$hadolintOpts ${1%% *}=${1##* }"
+                fi
+
+                shift
+            ;;
+
+            *)
+                break
+            ;;
+        esac
+    done
+
     for filepath in "$@" ; do
-        if ! hadolint "$filepath" ; then
+        if ! hadolint $hadolintOpts "$filepath" ; then
             error=1
         fi
     done
